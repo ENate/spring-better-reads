deb151449286fc5a97db8eb17ace4bce64b2e7e1
Manes Hi <manes.hipocrates@gmail.com>
2021-09-29 13:34:35 +0200

    added more content

---
+++ better-reads/spring-reads-data-loader/src/main/java/com/minejava/springreadsdataloader/SpringReadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.minejava.springreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReadsDataLoaderApplication.class, args);
+	}
+
+}
---
+++ better-reads/spring-reads-data-loader/src/test/java/com/minejava/springreadsdataloader/SpringReadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minejava.springreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
